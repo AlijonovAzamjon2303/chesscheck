a6105b137a49f9afa8e4f0601165ff296a9042d6
Azamjon <alijonovrobo@gmail.com>
2025-01-07 09:56:40 +0500

    bishop check

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ def pawn_check(x1, y1, x2, y2):
 def rook_check(x1, y1, x2, y2):
     return x1 == x2 or y1 == y2
 
+def bishop_check(x1, y1, x2, y2):
+    return abs(x1 - x2) == abs(y1 - y2)
 
 print(rook_check(1, 1, 8, 2))
