1ec33fe13d01644bc70593ea9911021261d2f412
Azamjon <alijonovrobo@gmail.com>
2025-01-07 09:32:54 +0500

    added pawn check

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print(12)
+def pawn_check(x1, y1, x2, y2):
+    return x1 == x2 and y2 - y1 == 1
+
+print(pawn_check(2, 3, 2, 3))
+
+# git init
+# git remote add origin link
+# git add .
+# git commit -m "words"
+# git push origin main
